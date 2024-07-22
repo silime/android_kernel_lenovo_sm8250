a5ba1f6cced63de482912831d741fb059017cf10
silime <60662669+silime@users.noreply.github.com>
2024-07-22 16:22:05 +0800

    [Fix] Ignore all magic mismatches Fix it by adding. 	flags |= MODULE_INIT_IGNORE_MODVERSIONS; 	flags |= MODULE_INIT_IGNORE_VERMAGIC; in `load_module`  method

--- kernel/module.c
+++ kernel/module.c
@@ -3716,6 +3716,9 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		err = -EPERM;
 		goto free_copy;
 	}
+	
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = module_sig_check(info, flags);
 	if (err)
